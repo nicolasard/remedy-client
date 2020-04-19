7b8bec4f45d7d9346e5989f2b59bd7ce00f78a0f
faloggs <Nicolas.Ardison@firstdata.com.ar>
2020-04-19 17:38:18 -0300

    Adding service

---
+++ src/main/java/ar/nic/remedy/services/IncidentsService.java
@@ -0,0 +1,28 @@
+package ar.nic.remedy.services;
+
+import ar.nic.remedy.dao.Incident;
+import ar.nic.remedy.wsclient.ClientConfiguration;
+import ar.nic.remedy.wsclient.clients.IncidentsClient;
+import ar.nic.remedy.wsclient.wsdl.GetListOutputMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class IncidentsService {
+
+    public List<Incident> getIncidents(ClientConfiguration clientConfiguration){
+        List<Incident> incidentList = new ArrayList<>();
+        IncidentsClient incidentsClient = new IncidentsClient(clientConfiguration);
+        GetListOutputMap incidents = incidentsClient.getIncidents();
+        for (GetListOutputMap.GetListValues incidentws: incidents.getGetListValues()) {
+            Incident incident = new Incident();
+            incident.setIncidentID(incidentws.getIncidentNumber());
+            incident.setSummary(incidentws.getSummary());
+            incidentList.add(incident);
+        }
+        return incidentList;
+    }
+}
