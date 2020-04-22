6e2619f98ef7de073929dc51fdbb54a665bb46fb
faloggs <Nicolas.Ardison@firstdata.com.ar>
2020-04-22 04:21:24 -0300

    Adding missing AppConfiguration class.

---
+++ src/main/java/ar/nic/remedy/AppConfiguration.java
@@ -0,0 +1,31 @@
+package ar.nic.remedy;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.constraints.NotBlank;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppConfiguration {
+    @NotBlank
+    private String user;
+    @NotBlank
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
